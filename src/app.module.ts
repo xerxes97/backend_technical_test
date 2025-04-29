8d2acfb938a0cdaefb6327693b799833c2877902
cristianQuev <119689796+cristianQuev@users.noreply.github.com>
2025-04-29 08:43:39 -0500

    users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database-provider/database-provider.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     DatabaseModule,
     ConfigModule.forRoot({ envFilePath: '.dev.env', isGlobal: true }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
